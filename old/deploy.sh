39566e86b83fdd5451eda81f26d31577e3b73b6c
Tilman Adler <tilman-git@t-animal.de>
2026-01-21 21:23:36 +0100

    repo: remove archived legacy app
    Delete the `old/` tree now that the new lib/ui replacement is in place.

--- old/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ssh uberspace rm -rf www/chores.t-animal.de/*
-scp -r build/* uberspace:www/chores.t-animal.de
-ssh uberspace chmod a+rX -R www/chores.t-animal.de/*
--- old/src/typings/configuration.d.ts
+++
@@ -1,7 +0,0 @@
-export type CleanUpTime = 'immediately' | 'when-due';
-
-export type Configuration = {
-  selectedCalendar: string | null;
-  backlogTimeSpan: number;
-  cleanUpTime: CleanUpTime;
-}
